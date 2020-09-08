fe82b4bc8419085cd84ab7a75e5ca456be0353d4
zhangzhengzhen <zhangzhengzhen@bytedance.com>
2020-09-08 16:13:43 +0800

    Improve documentation

--- Example/DCFrame/SimpleList/SimpleLabelCell.swift
+++ Example/DCFrame/SimpleList/SimpleLabelCell.swift
@@ -20,22 +20,17 @@ class SimpleLabelModel: DCCellModel {
 }
 
 class SimpleLabelCell: DCCell<SimpleLabelModel> {
-    private static let insets = UIEdgeInsets(top: 8, left: 15, bottom: 8, right: 15)
-    private static let font = UIFont.systemFont(ofSize: 17)
-    
     static let touch = DCEventID()
     
     let label: UILabel = {
         let label = UILabel()
-        label.backgroundColor = .clear
-        label.numberOfLines = 0
-        label.font = SimpleLabelCell.font
+        label.font = UIFont.systemFont(ofSize: 17)
         return label
     }()
 
-    private let separator: CALayer = {
+    let separateLine: CALayer = {
         let layer = CALayer()
-        layer.backgroundColor = UIColor(red: 200 / 255.0, green: 199 / 255.0, blue: 204 / 255.0, alpha: 1).cgColor
+        layer.backgroundColor = UIColor.lightGray.cgColor
         return layer
     }()
     
@@ -43,23 +38,23 @@ class SimpleLabelCell: DCCell<SimpleLabelModel> {
         super.setupUI()
         
         contentView.addSubview(label)
-        contentView.layer.addSublayer(separator)
+        contentView.layer.addSublayer(separateLine)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         let bounds = contentView.bounds
-        label.frame = bounds.inset(by: Self.insets)
-        let height: CGFloat = 0.5
-        let left = Self.insets.left
-        separator.frame = CGRect(x: left, y: bounds.height - height, width: bounds.width - left, height: height)
+        let left: CGFloat = 15
+        let height: CGFloat = 1.0 / UIScreen.main.scale
+        
+        label.frame = bounds.inset(by: UIEdgeInsets(top: 8, left: left, bottom: 8, right: 15))
+        separateLine.frame = CGRect(x: left, y: bounds.height - height, width: bounds.width - left, height: height)
     }
 
     override func cellModelDidUpdate() {
         super.cellModelDidUpdate()
         
         label.text = cellModel.text
-        selectionStyle = .default
     }
     
     override func didSelect() {
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <p align="center">
-  <img src="./Docs/Images/title.png" alt="title" width="800" />
+  <img src="./Guides/Images/title.png" alt="title" width="800" />
 </p>
 
 
@@ -58,7 +58,7 @@ github "bytedance/DCFrame" ~> 1.0.5
 
 We can quickly get started with DCFrame through the following examples:
 
-* [Simple List](./Docs/1. Simple List.md): How to create a simple list through DCFrame;
+* [Simple List](./Guides/1. Simple List.md): How to create a simple list through DCFrame;
 * [Post List](): Through this example we learn to create a combined list;
 * [Module Interaction]()：How to perform module event and data sharing in the post list
 * [More Examples](): We provide more examples, simply pull the git repo and run 'DCFrame.xcworkspace' in the 'Example' folder. 
